65cb217a44e5946ec3088eddf42b244e5235585e
MyMainPC <chajihun1171@gmail.com>
2025-11-03 22:28:21 +0900

    bird 애니메이션 추가

--- bird.py
+++ bird.py
@@ -7,9 +7,18 @@ class Bird:
         self.image = load_image('bird_animation.png')
         self.x, self.y = x, y
         self.frame = 0
+        self.dir = 1
+        self.flip = ''
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, 336, 138, 168, self.x, self.y)
+        self.image.clip_composite_draw((self.frame % 5) * 183, (2 - (self.frame // 5)) * 168, 180, 168, 0, self.flip, self.x, self.y, 100, 100)
 
     def update(self):
-        self.frame = (self.frame + 1) % 5
+        self.frame = (self.frame + 1) % 14
+        if self.x > 1600:
+            self.dir = -1
+            self.flip = 'h'
+        elif self.x < 0:
+            self.dir = 1
+            self.flip = ''
+        delay(0.1)
